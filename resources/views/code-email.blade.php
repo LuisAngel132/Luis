51f8b3acff9a6d7d563ae9f69562103978dc71c9
RafaSauBlas <RafaSauBlas@hotmail.com>
2022-03-27 11:28:39 -0600

    Segundo push

--- resources/views/code-email.blade.php
+++ resources/views/code-email.blade.php
@@ -4,7 +4,8 @@
     <title>Rafael INC incorporated asocieshon company</title>
 </head>
 <body>
-    <p>Este es su codigo de erificacion: <a href="{{ Storage::disk('spaces')->url($code) }}" download="$code">{{ Storage::disk('spaces')->url($code) }}</a></p>
-    <p>No lo comparta con nadie</p>
+    <p>Abra este enlace para acceder a su código: <a href="{{ Storage::disk('spaces')->url($code) }}" download="Prueba.txt">{{ Storage::disk('spaces')->url($code) }}</a></p>
+                                         <!-- <a href="download/acme-doc-2.0.1.txt" download="Acme Documentation (ver. 2.0.1).txt">Download Text</a> -->
+    <b><p>No comparte este enlace o código con nadie!!!</p></b>
 </body>
 </html>
